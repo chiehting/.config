d14017a05c89c787da6919653c9ec75a2dd73301
chiehting <ting911111@gmail.com>
2019-11-11 23:30:15 +0800

    feat: modified fish_function.fish, add grep subcommand to ssh function

--- fish/fish_function.fish
+++ fish/fish_function.fish
@@ -1,7 +1,12 @@
 # ssh default shell of tmux
 function ssh
-  if test "$argv" = "list" || test "$argv" = "ls"
-    cat ~/.ssh/**config | grep -i '^host'
+  set -l subcmd $argv[1]
+  if test "$subcmd" = "list" || test "$subcmd" = "ls"
+    set -l command "cat ~/.ssh/**config | grep -v '*' | grep -i '^host' -A 1"
+    for val in $argv[2..-1]
+      set command "$command | grep -i $val -A 1"
+    end
+    eval $command
   else
     /usr/bin/ssh -t $argv "tmux attach || tmux new || zsh || bash";
   end
