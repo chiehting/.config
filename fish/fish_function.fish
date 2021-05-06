37782742edd65cbf1e24a01fef5029c4169cf62e
chiehting <ting911111@gmail.com>
2021-05-06 22:53:40 +0800

    fix: slow command autocompletion

--- fish/fish_function.fish
+++ fish/fish_function.fish
@@ -52,3 +52,9 @@ function chrome-dev
   rm -rf $datadir
   open -n -a /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --args --user-data-dir="$datadir" --disable-web-security
 end
+
+
+# fix slow command autocompletion
+if test (uname) = Darwin
+  function __fish_describe_command; end
+end
